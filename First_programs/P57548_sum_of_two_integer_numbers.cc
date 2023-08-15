2201a5b7910308f999cf30fadcee82d9cc806dab
Camila Valentina Alonso Yepez <114765967+val12121@users.noreply.github.com>
2023-08-15 17:41:27 +0100

    Terminado

---
+++ First_programs/P57548_sum_of_two_integer_numbers.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main () {
+
+    int number1 {0};
+    int number2 {0};
+
+    std::cin >> number1 >> number2;
+    std::cout << std::endl; 
+    std::cout << number1 + number2;
+
+    return 0; 
+
+}
